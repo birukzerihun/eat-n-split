aa2bd0297a23ef0a773462e9e7c4581e30c699cd
Biruk Zerihun <birukz516@gmail.com>
2023-11-13 03:25:01 -0800

    update the reset

---
+++ src/FormAddFriend.js
@@ -0,0 +1,9 @@
+function FormAddFriend() {
+  return (
+    <form className='form-add-friend'>
+      <label>👫 Friend Name</label>
+      <input type='text' />
+      <label> Image URL</label>
+    </form>
+  )
+}
